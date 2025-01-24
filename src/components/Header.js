d9ff8f40c7a211015c53fb1095e0978eb12d826b
arif_meilanov <arif.m.04@mail.ru>
2025-01-24 21:40:45 +0300

    Header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
     render() {
         return (
             <header className="header">
                 {this.props.title}
+                <nav>
+                    <Link to="/task1">Task 1</Link>
+                    <Link to="/task2">Task 2</Link>
+                    <Link to="/task3">Task 3</Link>
+                    <Link to="/task4">Task 4</Link>
+                </nav>
             </header>
         )
     }
